435001466bd406fc84623996a5d598b157271f5a
admin <your-email=dvtung36@gmail.com>
2020-03-27 14:03:22 +0700

    Tạo Project

---
+++ QLKHO/FormLogin.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace QLKHO
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.lb = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.txtacc = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnDangnhap = new System.Windows.Forms.Button();
+            this.btnThoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lb
+            // 
+            this.lb.AutoSize = true;
+            this.lb.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb.Location = new System.Drawing.Point(277, 43);
+            this.lb.Name = "lb";
+            this.lb.Size = new System.Drawing.Size(374, 38);
+            this.lb.TabIndex = 0;
+            this.lb.Text = "Phần Mềm Quản Lý Kho";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(161, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "User ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(161, 227);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.ForeColor = System.Drawing.Color.Black;
+            this.checkBox1.Location = new System.Drawing.Point(326, 315);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 24);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Ghi nhớ";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.ForeColor = System.Drawing.Color.Black;
+            this.checkBox2.Location = new System.Drawing.Point(494, 315);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(139, 24);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Hiện mật khẩu";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // txtacc
+            // 
+            this.txtacc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.txtacc.Location = new System.Drawing.Point(326, 231);
+            this.txtacc.Margin = new System.Windows.Forms.Padding(4);
+            this.txtacc.Name = "txtacc";
+            this.txtacc.Size = new System.Drawing.Size(307, 26);
+            this.txtacc.TabIndex = 31;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox2.Location = new System.Drawing.Point(326, 141);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(307, 26);
+            this.textBox2.TabIndex = 32;
+            // 
+            // btnDangnhap
+            // 
+            this.btnDangnhap.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnDangnhap.FlatAppearance.BorderSize = 0;
+            this.btnDangnhap.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDangnhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnDangnhap.ForeColor = System.Drawing.Color.Black;
+            this.btnDangnhap.Image = ((System.Drawing.Image)(resources.GetObject("btnDangnhap.Image")));
+            this.btnDangnhap.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnDangnhap.Location = new System.Drawing.Point(284, 385);
+            this.btnDangnhap.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDangnhap.Name = "btnDangnhap";
+            this.btnDangnhap.Size = new System.Drawing.Size(123, 90);
+            this.btnDangnhap.TabIndex = 33;
+            this.btnDangnhap.Text = "Đăng nhập";
+            this.btnDangnhap.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnDangnhap.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnDangnhap.UseVisualStyleBackColor = true;
+            // 
+            // btnThoat
+            // 
+            this.btnThoat.AccessibleDescription = "";
+            this.btnThoat.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.btnThoat.FlatAppearance.BorderSize = 0;
+            this.btnThoat.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnThoat.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnThoat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.btnThoat.ForeColor = System.Drawing.Color.Black;
+            this.btnThoat.Image = ((System.Drawing.Image)(resources.GetObject("btnThoat.Image")));
+            this.btnThoat.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnThoat.Location = new System.Drawing.Point(550, 373);
+            this.btnThoat.Margin = new System.Windows.Forms.Padding(4);
+            this.btnThoat.Name = "btnThoat";
+            this.btnThoat.Size = new System.Drawing.Size(101, 90);
+            this.btnThoat.TabIndex = 34;
+            this.btnThoat.Text = "Thoát";
+            this.btnThoat.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnThoat.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnThoat.UseVisualStyleBackColor = true;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 517);
+            this.Controls.Add(this.btnThoat);
+            this.Controls.Add(this.btnDangnhap);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtacc);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb);
+            this.ForeColor = System.Drawing.Color.Maroon;
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.FormLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox txtacc;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnDangnhap;
+        private System.Windows.Forms.Button btnThoat;
+    }
+}
+
